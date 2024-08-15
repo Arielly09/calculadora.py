c74cf8751277d0e92ade5e9cb5eb22fffc9737de
Arielly09 <162440483+Arielly09@users.noreply.github.com>
2024-08-15 08:38:39 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,13 @@
+class Calculadora: 
+
+    def adicionar(self, a, b):
+        return a+b
+    
+    def subtrair(self, a, b):
+        return a-b
+    
+    def multiplicar(self, a, b):
+        return a*b
+    
+    def dividir(self, a, b):
+        if b==
